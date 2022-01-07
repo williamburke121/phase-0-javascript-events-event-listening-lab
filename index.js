f92a40365d4dbf989a479fba9892679f190514c5
William Warner Burke <williamburke121@gmail.com>
2022-01-07 01:01:26 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+function addingEventListener(click, clickAlert) {
+    const input = document.getElementById('input');
+    function clickAlert() {
+        alert ('I was clicked!');
+    }
+
+
+input.addEventListener('click', clickAlert)
 }
+function clickAlert;
